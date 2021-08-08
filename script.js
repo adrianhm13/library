1728efd0660199b67dfa85cd37a84db20e1c9756
Adrian Hervas <adrian.hervasmartinez@gmail.com>
2021-08-08 08:03:41 +0200

    Added basic DOM manipulation, Added basic CSS

--- script.js
+++ script.js
@@ -15,3 +15,14 @@ function addBookToLibrary(title, author, pages, read){
     const newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
 }
+
+function showBooks(myLibrary){ //Crearemos un DIV con estilos (modo tarjeta) y lo haremos aparecer cuando haya libros dentro de la array
+    for (let index = 0; index < myLibrary.length; index++) {
+        myLibrary[index].sayInfo();
+        
+        const divAllBooks = document.querySelector('.showBooks');  //Find div's books parent
+        const divBook = document.createElement('div');    //Create divs for showing the book's information
+        divBook.classList.add('cardBook')                 //Add the class to the card book
+        divAllBooks.appendChild(divBook);                 //Append the child to the parent's div
+    } 
+}
